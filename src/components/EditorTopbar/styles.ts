74d6851ef6d84443352f4404736b90f4202053a8
Diego Jimenez <dijim45@gmail.com>
2023-03-28 19:23:29 -0500

    feat: implement editable title on editor topbar

--- src/components/EditorTopbar/styles.ts
+++ src/components/EditorTopbar/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   background-color: ${({ theme }) => theme.color.NEUTRAL[300]};
   min-height: 40px;
-  padding: 10px 12px;
+  padding: 3px 12px;
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -14,10 +14,13 @@ export const Container = styled.div`
     transition: transform 0.3s;
     font-size: 20px;
     cursor: pointer;
+    flex-shrink: 0;
   }
 `;
 
 export const Content = styled.div`
   flex-grow: 1;
   font-weight: 500;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
